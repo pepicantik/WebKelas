acce710499552f7088ab462d664ee15b0696ed43
pepii imupppp <pepicantik5129@gmail.com>
2026-09-23 14:03:36 +0700

    feat:nambah layout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,8 +7,30 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function index()
     {
-        
+        if(session()->has('key')){
+            return redirect()->route('siswa.index');
+        }
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $key = Admin::where('username', $request->username)
+                    ->where('password', $request->password)
+                    ->get();
+
+        if(!$key->isEmpty()){
+            session()->put('key', $key);
+            return redirect()->route('siswa.index');
+        }
+        return redirect('/login');
+    }
+
+    public function logout()
+    {
+        session()->flush();
+        return redirect('/');
     }
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Techsoftone')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+
+    <nav class="navbar navbar-light bg-light mb-5">
+        <div class="container">
+            <a href="/" class="navbar-brand">Techsoftone</a>
+        @if (session()->has('key'))
+            <a href="/siswa" class="btn btn-outline-primary">Data Siswa</a>
+            <a href="/logout" class="btn btn-outline-primary">Logout</a>
+        @else
+            <a href="/login" class="btn btn-outline-primary">Login</a>
+        @endif
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('konten')
+    </div>
+
+
+    <footer class="text-body-secondary py-5">
+        <div class="container">
+            <p>&copy; 2026 | Techsoftone</p>
+        </div>
+    </footer>
+</body>
+</html>
