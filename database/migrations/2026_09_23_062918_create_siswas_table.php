517e3990bb3123eb17bd7d4799eb3cf3c71e961d
Verraajaa <verradao@gmail.com>
2026-09-23 13:15:50 +0700

    feat: migrate

---
+++ database/migrations/2026_09_23_062918_create_siswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap');
+            $table->date('tgl_lahir');
+            $table->string('tempat_lahir');
+            $table->string('hobi');
+            $table->string('image');
+            $table->enum('sosmed', ['tiktok', 'instagram']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
