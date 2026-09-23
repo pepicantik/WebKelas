01b47eac340d8da95ec97a37e53fb4668f39263c
pepii imupppp <pepicantik5129@gmail.com>
2026-09-23 13:19:35 +0700

    feat:model

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
-    //
+    protected $fillable = [
+        'nama_lengkap',
+        'tgl_lahir',
+        'tempat_lahir',
+        'hobi',
+        'image',
+        'sosmed'
+    ];
 }
