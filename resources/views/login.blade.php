cc4f34bc74b60552111091f6ccea80c92265c620
pepii imupppp <pepicantik5129@gmail.com>
2026-09-23 14:51:40 +0700

    feat:login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,10 +2,9 @@
 @section('title')
 @section('konten')
 
-    <div class="row-cols-md-3 g-3">
-        <div class="col">
+<div class="align-items-center justify-content-center">
             <div class="card shadow-sm">
-                <h2>Login</h2>
+                <h2 class="text-center mt-4">Login</h2>
             <div class="card-body">
                 <form action="{{ route('login') }}" method="post">
                     @csrf
@@ -16,10 +15,9 @@
                     <label>Username</label>
                     <input class="form-control mt-2 mb-3" type="text" name="username" id="" placeholder="username" required>
 
+                    <button type="submit" class="form-control mt-2 mb-3 btn btn-primary">Login</button>
                 </form>
             </div>
             </div>
-        </div>
-    </div>
-
+</div>
 @endsection
