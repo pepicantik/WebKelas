3f476d8785e843ccc322ccfb2960148abf23f688
pepii imupppp <pepicantik5129@gmail.com>
2026-09-24 14:51:33 +0700

    feat:gatau lupa

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -3,18 +3,77 @@
 
 @section('konten')
 
+<style>
+    .card-body{
+        padding: 40px;
+    }
+    .custom-card{
+        border: 0;
+        background-color: #ffff;
+        border-radius: 15px;
+        box-shadow: 0 5px 5px 5px rgb(0, 0, 0, 0.05);
+        overflow: hidden;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        width: 100%;
+    }
+    .custom-card-header{
+        padding: 25px 20px;
+        border-bottom: none;
+    }
+    .custom-container{
+        width: 100%;
+        min-height: 100vh;
+    }
+    .short-line{
+        border: none;
+        border-top: 2px solid #2C3947;
+        width: 650px;
+        margin: 0 0 16px 0;
+    }
+    .btn-back{
+        font-weight: 600;
+        border-radius: 8px;
+        padding-left: 30px;
+        padding-right: 30px;
+        border-color: #FCAD38;
+        color: #FCAD38;
+        border-width: 2px;
+    }
+    .btn-back:hover{
+        background-color: #FCAD38;
+        font-weight: 600;
+        padding: 12px;
+        border-radius: 12px;
+        color: #EAECF0;;
+        border-width: 2px;
+    }
+</style>
+<div class="container custom-container table-responsive">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card custom-card">
+                <div class="card-body">
     <div class="d-flex gap-5 align-items-start mb-5">
         <img src="{{ url('storage/'. $siswa->image) }}" alt="" height="300px">
         <div>
             <h2 class="mb-4">{{ $siswa->nama_lengkap }}</h2>
             <p class="mb-4"><strong>TTL :</strong> {{ $siswa->tempat_lahir }}, {{ $siswa->tgl_lahir }}</p>
+            <hr class="short-line">
             <p class="mb-4"><strong>Hobi :</strong> {{ $siswa->hobi }}</p>
+            <hr class="short-line">
             <p class="mb-4"><strong>Sosmed :</strong> {{ $siswa->sosmed }}</p>
         </div>
     </div>
     @if (session()->has('key'))
-        <a href="{{ route('siswa.index') }}" class="btn btn-outline-secondary">Kembali</a>
+        <a href="{{ route('siswa.index') }}" class="btn btn-back">Kembali</a>
     @else
-        <a href="/" class="btn btn-outline-secondary">Kembali</a>
+        <a href="/" class="btn btn-back">Kembali</a>
     @endif
+
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,22 +7,84 @@
     <title>@yield('title', 'Techsoftone')</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
+<style>
+    nav.custom-nav {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 6%;
+  background: linear-gradient(
+    to right,
+    rgba(31, 57, 211, 0.95) 0%,
+    rgba(42, 70, 220, 0.9) 50%,
+    rgba(91, 128, 245, 0.85) 100%
+  );
+  box-shadow: 0 10px 20px 10px rgba(0, 0, 0, 0.05);
+}
+
+    .logo {
+      color: white;
+      font-size: 22px;
+      font-weight: 800;
+      letter-spacing: 1px;
+      text-decoration: none;
+    }
+
+    .nav-links {
+      display: flex;
+      align-items: center;
+      gap: 25px;
+      list-style: none;
+      margin: 0;
+    }
+
+    .nav-links a {
+      color: white;
+      text-decoration: none;
+      font-size: 13px;
+      font-weight: 700;
+      letter-spacing: 1px;
+      transition: 0.3s;
+    }
+
+    .nav-links a:hover {
+      opacity: 0.7;
+    }
+
+    .login-btn {
+      text-decoration: none;
+      color: #3152dc;
+      background: white;
+      padding: 9px 14px;
+      border-radius: 6px;
+      font-size: 12px;
+      font-weight: 800;
+      box-shadow: 0 8px 25px rgba(0, 0, 0, 0.15);
+      transition: 0.3s;
+    }
+
+    .login-btn:hover {
+      transform: translateY(-3px);
+      box-shadow: 0 12px 30px rgba(0, 0, 0, 0.2);
+    }
+</style>
 <body>
 
-    <nav class="navbar navbar-light bg-light mb-5">
-        <div class="container">
-            <a href="/" class="navbar-brand">Techsoftone</a>
+    <nav class="custom-nav">
+        <a href="/" class="logo">Techsoftone</a>
+
         @if (session()->has('key'))
-        <div class="">
-            <a href="/home" class="btn btn-outline-primary">Beranda</a>
-            <a href="/moment" class="btn btn-outline-primary">Moment</a>
-            <a href="/siswa" class="btn btn-outline-primary">Data Siswa</a>
-            <a href="/logout" class="btn btn-outline-primary">Logout</a>
-        </div>
+            <ul class="nav-links">
+                <li><a href="/home">Beranda</a></li>
+                <li><a href="/moment">Moment</a></li>
+                <li><a href="/siswa">Data Siswa</a></li>
+                <li><a href="/logout">Logout</a></li>
+            </ul>
         @else
-            <a href="/login" class="btn btn-outline-primary">Login</a>
+            <a href="/login" class="login-btn">LOGIN</a>
         @endif
-        </div>
     </nav>
 
     <div class="container">
